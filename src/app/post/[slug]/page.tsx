9732a1345cdd92621f32ab537041a4dbca9fb40e
pyyupsk <pyyupsk@proton.me>
2024-08-22 21:30:21 +0700

    fix: enhance metadata for post page and handle missing post case

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -14,7 +14,20 @@ type Props = {
 export async function generateMetadata({ params: { slug } }: Props) {
     const post = await prisma.post.findUnique({ where: { id: slug } });
 
-    if (!post) return commonMetaData;
+    if (!post) {
+        return commonMetaData({
+            title: "Page Not Found – Explore More from First",
+            description:
+                "Oops! It looks like the page you're looking for doesn't exist. Head back to explore other projects, blog posts, and insights from First.",
+        });
+    }
+
+    const metaData = commonMetaData({
+        title: `${post.title}  – Insights from First's Programming Journey`,
+        description: `Read "${post.title}" by First. Discover insights, challenges, and experiences in programming. Explore this detailed blog post on https://pyyupsk.vercel.app.`,
+    });
+
+    return metaData;
 }
 
 export default async function Page({ params }: Props) {
