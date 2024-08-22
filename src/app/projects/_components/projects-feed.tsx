2c66c43ca357205d41e45d9d0d0cec21aa0bfd4b
pyyupsk <pyyupsk@proton.me>
2024-08-22 19:54:49 +0700

    fix(projects-feed): order projects by ID instead of start date

--- src/app/projects/_components/projects-feed.tsx
+++ src/app/projects/_components/projects-feed.tsx
@@ -4,7 +4,7 @@ import { ProjectsList } from "./projects-list";
 export async function ProjectsFeed() {
     const projects = await prisma.project.findMany({
         orderBy: {
-            startDate: "desc",
+            id: "desc",
         },
     });
 
