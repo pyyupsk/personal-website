88121260be9476db0be0150d9413838c26af3274
pyyupsk <pyyupsk@proton.me>
2024-08-22 21:19:00 +0700

    style(post-card): update title style in post card

--- src/app/posts/_components/post-card.tsx
+++ src/app/posts/_components/post-card.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function PostCard({ post }: { post: Omit<Post, "content" | "status"> }) {
     return (
         <Link href={`/post/${post.id}`}>
-            <h3 className="text-base text-foreground">{post.title}</h3>
+            <h3 className="text-foreground">{post.title}</h3>
             <p>{format(post.publishDate, "LLLL d, yyyy")}</p>
             {post.description && <p className="mt-1.5">{post.description}</p>}
         </Link>
