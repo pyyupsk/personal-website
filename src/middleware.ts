eca146d057c6807cc3b077156ce119632ca512e4
pyyupsk <pyyupsk@proton.me>
2024-08-22 15:21:05 +0700

    refactor(middleware): simplify middleware by removing custom logic and user upsert

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,43 +1,6 @@
-import { prisma } from "@/lib/prisma";
-import { clerkClient, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
-
-export default clerkMiddleware(async (auth, request) => {
-    if (!isPublicRoute(request)) {
-        auth().protect();
-    }
-
-    const userId = auth().userId;
-
-    if (!userId) return;
-
-    const user = await clerkClient().users.getUser(userId);
-
-    if (!user) return;
-
-    try {
-        await prisma.user.upsert({
-            where: { id: userId },
-            create: {
-                id: userId,
-                email: user.emailAddresses[0].emailAddress,
-            },
-            update: {},
-        });
-    } catch (error) {
-        if (error instanceof Error) {
-            if (error.message.includes("Unique constraint failed")) {
-                console.log("User already exists in the database. Ignoring error.");
-            } else {
-                throw error;
-            }
-        } else {
-            console.error("Unexpected error type:", error);
-            throw new Error("Unexpected error occurred");
-        }
-    }
-});
+export default clerkMiddleware();
 
 export const config = {
     matcher: [
