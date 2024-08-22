66ba911e6de75067a82842b122d3fd26cfb5aad9
pyyupsk <pyyupsk@proton.me>
2024-08-22 19:54:56 +0700

    style(header): adjust header title size for better readability

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,21 +2,16 @@
 
 import { navigation } from "@/constants/navigation";
 import Link from "next/link";
-import { notFound, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export function Header() {
     const pathname = usePathname();
 
     const page = navigation.find((page) => page.href === pathname);
 
-    if (!page) {
-        notFound();
-        return null;
-    }
-
     return (
         <div className="container flex flex-col items-center justify-between gap-3 md:flex-row pt-20">
-            <h1>{page.name}</h1>
+            <h1 className="text-2xl">{page?.name}</h1>
             <ul className="flex items-center gap-3">
                 {navigation.map((item) => (
                     <li key={item.name}>
