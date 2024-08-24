5da00545c9eb7f45bf42237f2cd42b587ff374f6
pyyupsk <pyyupsk@proton.me>
2024-08-24 23:21:02 +0700

    chore(config): update next.js config to allow GitHub avatar images

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
                 protocol: "https",
                 hostname: "geon6kvypdtg3nc1.public.blob.vercel-storage.com",
             },
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/u/**",
+            },
         ],
     },
 };
