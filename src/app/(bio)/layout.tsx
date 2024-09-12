576e068173b936ddd3a58d2353f1863b7874e660
pyyupsk <pyyupsk@proton.me>
2024-09-12 18:00:18 +0700

    feat: update bio page metadata with dynamic OG image

--- src/app/(bio)/layout.tsx
+++ src/app/(bio)/layout.tsx
@@ -1,9 +1,16 @@
 import { commonMetaData } from '@/lib/meta';
+import { openGraph } from '@/lib/open-graph';
 
 export function generateMetadata() {
     const metaData = commonMetaData({
         description:
             'Meet Pongsakorn Thipayanate, a self-taught coder passionate about Node.js and web development. Explore my projects, blog, and connect via GitHub, Twitter, or email.',
+        image: openGraph({
+            button: 'View My Work',
+            description:
+                'Discover more about Pongsakorn Thipayanate, an independent programmer specializing in Node.js and web solutions.',
+            title: 'Pongsakorn Thipayanate',
+        }),
         title: 'Pongsakorn Thipayanate | Programmer & Web Developer',
     });
 
