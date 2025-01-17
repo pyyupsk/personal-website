09c09397921bc2ea08e661298493308217fbe44f
pyyupsk <pyyupsk@proton.me>
2025-01-17 20:22:56 +0700

    feat(markdown): add language color coding to code blocks

---
+++ src/constants/language-colors.ts
@@ -0,0 +1,15 @@
+export const languageColors: { [key: string]: string } = {
+    bash: '#89E051',
+    csharp: '#178600',
+    go: '#00ADD8',
+    javascript: '#F1E05A',
+    jsx: '#F1E05A',
+    lua: '#000080',
+    prisma: '#5764D2',
+    python: '#3572A5',
+    rust: '#DEA584',
+    sh: '#89E051',
+    sql: '#E38C00',
+    tsx: '#2B7489',
+    typescript: '#2B7489',
+};
--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,20 +1,41 @@
+import { languageColors } from '@/constants/language-colors';
 import { marked } from 'marked';
 import markedShiki from 'marked-shiki';
 import { cache } from 'react';
 import { codeToHtml } from 'shiki';
 
 const parser = cache(async (markdown: string) => {
-    const html = marked
+    marked.use({
+        renderer: {
+            code({ lang, text }) {
+                return `___CODE_BLOCK_START___${lang || ''}___${text}___CODE_BLOCK_END___`;
+            },
+        },
+    });
+
+    const html = await marked
         .use(
             markedShiki({
                 async highlight(code, lang) {
-                    return await codeToHtml(code, {
+                    const highlighted = await codeToHtml(code, {
                         lang,
                         themes: {
                             dark: 'min-dark',
                             light: 'min-light',
                         },
                     });
+
+                    if (!lang) return highlighted;
+
+                    const langColor = languageColors[lang.toLowerCase()] || '#303036';
+
+                    return `<figure>
+                        <figcaption class="code-title flex items-center rounded-t-md border border-b-0 border-border bg-card p-3 font-mono text-card-foreground">
+                            <span class="mr-1.5 w-[10px] h-[10px] rounded-full" style="background-color: ${langColor}"></span>
+                            <span class='flex-grow'>${lang}</span>
+                        </figcaption>
+                        ${highlighted}
+                    </figure>`;
                 },
             }),
         )
